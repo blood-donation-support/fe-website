19af911f319b42cc7fa3ee11780d6a22dc0143a3
NgynThao <thaonttse173208@fpt.edu.vn>
2025-07-24 13:29:22 +0700

    update list user admin

--- src/types/user.ts
+++ src/types/user.ts
@@ -5,9 +5,11 @@ export interface User {
   date_of_birth: string; 
   role: "Admin" | "Staff" | "Staff-Warehouse" | string;
   gender: "Male" | "Female" | "Other" | string;
+  citizen_id_number: string;
   blood_group_id: string | null;
   number_of_donation: number;
   number_of_request: number;
+  address: string;
   weight: number;
   location: string;
   phone: string;
@@ -16,6 +18,9 @@ export interface User {
   updated_at: string; 
   forgot_password_token: "string"
 }
+ 
+   
+   
 export interface NewUserPayload {
   citizen_id_number: string;
   phone: string;
